1b8f487c5e0a515d9c3cd66104cf60d779a73120
Henry Galvez <soulofhenry@gmail.com>
2020-11-13 06:34:18 -0600

    se quito bcrypt backend

--- Backend/src/controllers/user.js
+++ Backend/src/controllers/user.js
@@ -1,33 +1,36 @@
 const gererateToken = require('../security/verify').generateToken;
 const User = require('../models/User');
-var bcrypt = require('bcrypt');
+//var bcrypt = require('bcrypt');
 
 module.exports = {
 
     signup: async (req, res, next) => {
         const { email, password, name, username } = req.body
-        bcrypt.hash(password, 10, async (error, hash) => {
-            if (error) {
-                res.status(500).json({ message: 'Erro to create user' })
-            }
-            const newUser = new User({ email, password: hash, name, username })
+        //bcrypt.hash(password, 10, async (error, hash) => {
+            //if (error) {
+            //    res.status(500).json({ message: 'Erro to create user' })
+            //}
+            //const newUser = new User({ email, password: hash, name, username })
+            const newUser = new User({ email, password, name, username })
             await newUser.save()
             const token = gererateToken(newUser);
             res.status(200).json({ token: token, message: 'Successfully!!' })
-        });
+        //});
     },
     signin: async (req, res, next) => {
         const { email, password } = req.body
         const user = await User.findOne({ email })
         if (!user) return res.status(400).send({ message: "this email does not exists" })
-        bcrypt.compare(password, user.password, function (err, result) {
-            if (result == true) {
+        if (!user.password != password) res.status(401).send({ message: 'Wrong password' })
+        return res.status(200).json({ token: token, message: "Successfully!!" })
+        //bcrypt.compare(password, user.password, function (err, result) {
+           /* if (result == true) {
                 const token = gererateToken(user);
                 return res.status(200).json({ token: token, message: "Successfully!!" })
             } else {
                 res.status(401).send({ message: 'Wrong password' })
-            }
-        });
+            }*/
+        //});
     },
     info: async (req, res, next) => {
         const { idUser } = req.body
